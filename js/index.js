741b3f6e4d8d876176b0ff2c0dd2095101444335
Kromus22 <kromus2@gmail.com>
2021-11-10 11:10:39 +0500

    +menu, закрытие по клику мимо, блок скрола

--- js/index.js
+++ js/index.js
@@ -1,41 +1,98 @@
-const presentOrderBtn = document.querySelector('.present__order-btn')
-const pageOverlayModal = document.querySelector('.page__overlay_modal')
-const modalClose = pageOverlayModal.querySelector('.modal__close')
+const disabledScroll = () => {
 
+    const widthScroll = window.innerWidth - document.body.offsetWidth
 
-const handlerModal = (openBtn, modal, openSelector, closeTrigger, sk) => {
+    document.body.scrollPosition = window.scrollY
 
-    let opacity = 0
-
-    const speed = {
-        slow: 10,
-        medium: 5,
-        fast: 1,
-        default: 3,
-    }
-
-    openBtn.addEventListener('click', () => {
-        modal.style.opacity = opacity
-
-        modal.classList.add(openSelector)
-
-        const timer = setInterval(() => {
-            opacity += 0.02
-            modal.style.opacity = opacity
-            if (opacity >= 1) clearInterval(timer)
-        }, speed[sk] ? speed[sk] : speed.medium)
-    })
-
-    closeTrigger.addEventListener('click', () => {
-        const timer = setInterval(() => {
-            opacity -= 0.02
-            modal.style.opacity = opacity
-            if (opacity <= 0) {
-                clearInterval(timer)
-                modal.classList.remove(openSelector)
-            }
-        }, speed[sk] ? speed[sk] : speed.medium)
-    })
+    document.body.style.cssText = `
+    overflow: hidden;
+    position: fixed;
+    top: -${document.body.scrollPosition}px;
+    left: 0;
+    height: 100vh;
+    width: 100vw;
+    padding-right: ${widthScroll}px;
+    `
 }
 
-handlerModal(presentOrderBtn, pageOverlayModal, 'page__overlay_modal_open', modalClose, 'medium')
+const enabledScroll = () => {
+    document.body.style.cssText = 'position: relative'
+    window.scroll({ top: document.body.scrollPosition })
+}
+
+
+{
+    const presentOrderBtn = document.querySelector('.present__order-btn')
+    const pageOverlayModal = document.querySelector('.page__overlay_modal')
+    const modalClose = pageOverlayModal.querySelector('.modal__close')
+
+
+    const handlerModal = (openBtn, modal, openSelector, closeTrigger, sk) => {
+
+        let opacity = 0
+
+        const speed = {
+            slow: 10,
+            medium: 5,
+            fast: 1,
+            default: 3,
+        }
+
+        const openModal = () => {
+            disabledScroll()
+            modal.style.opacity = opacity
+
+            modal.classList.add(openSelector)
+
+            const timer = setInterval(() => {
+                opacity += 0.02
+                modal.style.opacity = opacity
+                if (opacity >= 1) clearInterval(timer)
+            }, speed[sk] ? speed[sk] : speed.medium)
+        }
+
+        const closeModal = () => {
+            enabledScroll()
+            const timer = setInterval(() => {
+                opacity -= 0.02
+                modal.style.opacity = opacity
+                if (opacity <= 0) {
+                    clearInterval(timer)
+                    modal.classList.remove(openSelector)
+                }
+            }, speed[sk] ? speed[sk] : speed.medium)
+        }
+
+        openBtn.addEventListener('click', openModal)
+
+        closeTrigger.addEventListener('click', closeModal)
+
+        modal.addEventListener('click', (event) => {
+            if (event.target === modal) {
+                closeModal()
+            }
+        })
+    }
+
+    handlerModal(presentOrderBtn, pageOverlayModal, 'page__overlay_modal_open', modalClose, 'medium')
+}
+
+
+{
+    const headerContacts = document.querySelector('.header__contacts')
+    const headerContactsBurger = document.querySelector('.header__contacts-burger')
+
+    const heandlerBurger = (openBtn, menu, openSelector) => {
+        openBtn.addEventListener('click', () => {
+            if (menu.classList.contains(openSelector)) {
+                menu.style.height = ''
+                menu.classList.remove(openSelector)
+            } else {
+                menu.style.height = menu.scrollHeight + 'px'
+                menu.classList.add(openSelector)
+            }
+        })
+    }
+
+    heandlerBurger(headerContactsBurger, headerContacts, 'header__contacts_open')
+}
